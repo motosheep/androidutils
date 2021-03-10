85f32993f3aa228c30923512298256720feaad64
li <2487177596@qq.com>
2021-03-10 11:40:07 +0800

    1增加自动旋转ImageView

--- app/src/main/java/com/north/light/androidutils/MainActivity.java
+++ app/src/main/java/com/north/light/androidutils/MainActivity.java
@@ -7,94 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.north.light.androidutils.colordraw.SadDrawUtils;
 import com.north.light.androidutils.download.DownloadManager;
+import com.north.light.androidutils.imageview.RotateImageView;
 import com.north.light.androidutils.textview.NumAnimTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SadDrawUtils.getInstance().setSadTheme(this);
+        initView();
+    }
 
-//        BannerViewPager viewPager = findViewById(R.id.banner);
-//        List<String> a = new ArrayList<>();
-//        a.add("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1021768252,432753213&fm=26&gp=0.jpg");
-//        a.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1596688315897&di=551f51d38487e1a62fbdeec2c00c147b&imgtype=0&src=http%3A%2F%2Fattachments.gfan.com%2Fforum%2F201608%2F04%2F2339412b3z30zbd2j6k652.jpg");
-//        a.add("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1021768252,432753213&fm=26&gp=0.jpg");
-//        a.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1596688315897&di=551f51d38487e1a62fbdeec2c00c147b&imgtype=0&src=http%3A%2F%2Fattachments.gfan.com%2Fforum%2F201608%2F04%2F2339412b3z30zbd2j6k652.jpg");
-//        a.add("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1021768252,432753213&fm=26&gp=0.jpg");
-//        a.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1596688315897&di=551f51d38487e1a62fbdeec2c00c147b&imgtype=0&src=http%3A%2F%2Fattachments.gfan.com%2Fforum%2F201608%2F04%2F2339412b3z30zbd2j6k652.jpg");
-//        viewPager.setImageView(a, new BannerViewPager.LoadImageListener() {
-//            @Override
-//            public void loadImage(String url, ImageView pic) {
-//                pic.setScaleType(ImageView.ScaleType.CENTER_CROP);
-//                Glide.with(MainActivity.this).load(url).override(200, 200).into(pic);
-//            }
-//        });
-
-//        VerticalScrollTxView scrollTxView = findViewById(R.id.scrollText);
-//        List<String> mShowList = new ArrayList<>();
-//        for (int i = 0; i < 2; i++) {
-//            mShowList.add("位置: " + i);
-//        }
-//        scrollTxView.setTextList(mShowList);
-//
-//        //流式布局
-//        FlowLayout flowLayout = findViewById(R.id.flowlayout);
-//        List<String> list = new ArrayList<>();
-//        list.add("java");
-//        list.add("javaEE");
-//        list.add("javaME");
-//        list.add("c");
-//        list.add("php");
-//        list.add("ios");
-//        list.add("c++");
-//        list.add("c#");
-//        list.add("Android");
-//        flowLayout.setAlignByCenter(FlowLayout.AlienState.LEFT);
-//        flowLayout.setAdapter(list, R.layout.flow_item, new FlowLayout.ItemView<String>() {
-//            @Override
-//            public void getCover(String item, FlowLayout.ViewHolder holder, View inflate, int position) {
-//                holder.setText(R.id.tv_label_name,item);
-//            }
-//        });
+    private void initView() {
 
     }
 
-
-
-
-    public void start(View view) {
-
-        DownloadManager.getInstance().init(this.getApplicationContext());
-        DownloadManager.getInstance().start("http://codown.youdao.com/note/youdaonote_android_7.0.1_youdaoweb.apk",
-                new DownloadManager.DataBackInfo() {
-
-
-                    @Override
-                    public void data(long total, long current, float percent, boolean isFinish) {
-                        Log.d("TAG----","total: " + total + "\tcurrent: " + current
-                                + "\tpercent: " + percent  + "\tisFinish: " + isFinish);
-                    }
-
-                    @Override
-                    public void error(String message) {
-                        Log.d("TAG----","error: " + message);
-                    }
-                });
-    }
-
-    public void end(View view) {
-        DownloadManager.getInstance().stop("http://codown.youdao.com/note/youdaonote_android_7.0.1_youdaoweb.apk");
-    }
-
-
-    public void animnum(View view) {
-       NumAnimTextView nu =  findViewById(R.id.numanim);
-       nu.setAnimText("" + (Math.random()*1000));
-    }
 }
