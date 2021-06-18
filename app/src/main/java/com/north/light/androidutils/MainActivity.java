b9a4104d3e289a5f066e5d7b9657a831ab49e50b
li <2487177596@qq.com>
2021-06-18 15:50:29 +0800

    集成xaop

--- app/src/main/java/com/north/light/androidutils/MainActivity.java
+++ app/src/main/java/com/north/light/androidutils/MainActivity.java
@@ -2,13 +2,17 @@ package com.north.light.androidutils;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.north.light.androidutils.imageview.FlowLikeView;
+import com.north.light.androidutils.aspectj.AspectJManager;
 import com.north.light.androidutils.imageview.RotateImageView;
+import com.xuexiang.xaop.XAOP;
+import com.xuexiang.xaop.annotation.Intercept;
+
+import static com.north.light.androidutils.aspectj.AspectJConstant.INTERCEPT_LOGIN;
 
 public class MainActivity extends AppCompatActivity {
     int type = 1;
@@ -20,11 +24,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        AspectJManager.getInstance();
     }
 
-    private void initView() {
+    public void initView() {
         final RotateImageView like = findViewById(R.id.activity_main_rotate);
-       like.startAnim();
+//       like.startAnim();
+
+
+        //X AOP
+        XAOP.init(this.getApplication()); //初始化插件
+        XAOP.debug(true); //日志打印切片开启
+        XAOP.setPriority(Log.INFO); //设置日志打印的等级,默认为0
     }
 
+    @Intercept(INTERCEPT_LOGIN)
+    public void test(View view) {
+        System.out.println("@test");
+    }
 }
---
+++ app/src/main/java/com/north/light/androidutils/aspectj/AspectJConstant.java
@@ -0,0 +1,12 @@
+package com.north.light.androidutils.aspectj;
+
+/**
+ * Created by lzt
+ * time 2021/6/18 15:35
+ *
+ * @author lizhengting
+ * 描述：
+ */
+public class AspectJConstant {
+    public static final int INTERCEPT_LOGIN = 10;
+}
