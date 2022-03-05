998590d5a998e67b99003657dd82595d744435b2
lzt <2487177596@qq.com>
2022-03-05 09:26:59 +0800

    1图片水印修正

--- app/src/main/java/com/north/light/androidutils/water/WaterActivity.kt
+++ app/src/main/java/com/north/light/androidutils/water/WaterActivity.kt
@@ -1,5 +1,6 @@
 package com.north.light.androidutils.water
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
@@ -14,6 +15,7 @@ import com.north.light.androidutils.water.water.params.ImgWaterOrg
 
 /**
  * 水印activity
+ * 包含水印与图片压缩逻辑
  * */
 class WaterActivity : AppCompatActivity() {
     private val TAG = WaterActivity::class.java.simpleName
@@ -40,43 +42,50 @@ class WaterActivity : AppCompatActivity() {
         val path = Environment.getExternalStorageDirectory().path + "/001.jpg"
         val result = Compress.Builder().with(this).load(path).launch()
         Log.d(TAG, "压缩路径:$result")
-
-        var waterResult :MutableList<String> = ArrayList()
+        mTargetImg.setImageBitmap(BitmapFactory.decodeFile(result.first()))
+        var waterResult: MutableList<String> = ArrayList()
         //event------------------------------------------------------------
         mRg.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
             override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+                if (result.isNullOrEmpty()) {
+                    return
+                }
                 when (checkedId) {
                     R.id.activity_water_topleft -> {
                         //左上角水印
                         waterResult = ImgWaterUtils.Builder()
-                            .with(this@WaterActivity)
-                            .source(result.first(), R.mipmap.ic_heart1,ImgWaterOrg.LEFT_TOP)
-                            .launch()
+                                .with(this@WaterActivity)
+                                .source(result.first(), R.mipmap.ic_heart1, ImgWaterOrg.LEFT_TOP)
+                                .launch()
                         Log.d(TAG, "水印路径:${waterResult.firstOrNull()}")
+                        mTargetImg.setImageBitmap(BitmapFactory.decodeFile(waterResult.first()))
                     }
                     R.id.activity_water_topright -> {
                         //右上角水印
                         waterResult = ImgWaterUtils.Builder()
-                            .with(this@WaterActivity)
-                            .source(result.first(), R.mipmap.ic_heart1,ImgWaterOrg.RIGHT_TOP)
-                            .launch()
+                                .with(this@WaterActivity)
+                                .source(result.first(), R.mipmap.ic_heart1, ImgWaterOrg.RIGHT_TOP)
+                                .launch()
                         Log.d(TAG, "水印路径:${waterResult.firstOrNull()}")
+                        mTargetImg.setImageBitmap(BitmapFactory.decodeFile(waterResult.first()))
                     }
                     R.id.activity_water_btleft -> {
                         //左下角水印
                         waterResult = ImgWaterUtils.Builder()
-                            .with(this@WaterActivity)
-                            .source(result.first(), R.mipmap.ic_heart1,ImgWaterOrg.LEFT_BOTTOM)
-                            .launch()
+                                .with(this@WaterActivity)
+                                .source(result.first(), R.mipmap.ic_heart1, ImgWaterOrg.LEFT_BOTTOM)
+                                .launch()
                         Log.d(TAG, "水印路径:${waterResult.firstOrNull()}")
+                        mTargetImg.setImageBitmap(BitmapFactory.decodeFile(waterResult.first()))
                     }
                     R.id.activity_water_btRight -> {
                         //右下角水印
                         waterResult = ImgWaterUtils.Builder()
-                            .with(this@WaterActivity)
-                            .source(result.first(), R.mipmap.ic_heart1,ImgWaterOrg.RIGHT_BOTTOM)
-                            .launch()
+                                .with(this@WaterActivity)
+                                .source(result.first(), R.mipmap.ic_heart1, ImgWaterOrg.RIGHT_BOTTOM)
+                                .launch()
                         Log.d(TAG, "水印路径:${waterResult.firstOrNull()}")
+                        mTargetImg.setImageBitmap(BitmapFactory.decodeFile(waterResult.first()))
                     }
                 }
             }
